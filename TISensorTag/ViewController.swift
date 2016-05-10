1d424f928c48f5e76ca63f278c47e3b73cdc79ab
AndreMuis <Andre@AndreMuis.com>
2016-05-09 20:41:42 -0700

    Converting to Swift.

---
+++ TISensorTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TISensorTag
+//
+//  Created by Andre Muis on 5/9/16.
+//  Copyright © 2016 Andre Muis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
